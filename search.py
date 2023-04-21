07ea2603ddc4b888fa6c5214f4dea5c491f30be8
TsugawaNaoki0 <104245092+TsugawaNaoki0@users.noreply.github.com>
2023-04-21 21:33:49 +0900

    Add files via upload

--- search.py
+++ search.py
@@ -18,11 +18,16 @@ word_aaa = aaa.apple(args)
 if (word_aaa == "りんご"):
     print("<a href='https://ja.wikipedia.org/wiki/%E3%83%AA%E3%83%B3%E3%82%B4'>" + str(word_aaa) + "</a><br><br>")
 
-
-
-
+# 00000000000000000000000000000000000000000000000000000000000000000000000000000000
 bbb = word_garage.jimintou_class()
 word_bbb = bbb.jimintou(args)
 
 if (word_bbb == "自民党"):
     print("<a href='https://www.jimin.jp/'>" + str(word_bbb) + "</a><br><br>")
+
+# 00000000000000000000000000000000000000000000000000000000000000000000000000000000
+ccc = word_garage.lazarus_class()
+word_ccc = ccc.lazarus(args)
+
+if (word_ccc == "ラザルス"):
+    print("<a href='./box/lazarus.html'>" + str(word_ccc) + "</a><br><br>")
