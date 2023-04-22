6bee41bf64c30f75ef4d2bae901bb04a84b772d5
TsugawaNaoki0 <104245092+TsugawaNaoki0@users.noreply.github.com>
2023-04-22 09:46:31 +0900

    Add files via upload

--- search.py
+++ search.py
@@ -12,6 +12,19 @@ args = sys.argv[1]
 print("<br>")
 print("<br>")
 
+
+
+
+# for i in range(len(args)+1):
+#     for v in itertools.permutations(args, i):
+#         # print(v)
+#         # print(''.join(v))
+#         args = ''.join(v)
+        
+
+
+
+
 aaa = word_garage.apple_class()
 word_aaa = aaa.apple(args)
 
@@ -19,15 +32,29 @@ if (word_aaa == "りんご"):
     print("<a href='https://ja.wikipedia.org/wiki/%E3%83%AA%E3%83%B3%E3%82%B4'>" + str(word_aaa) + "</a><br><br>")
 
 # 00000000000000000000000000000000000000000000000000000000000000000000000000000000
-bbb = word_garage.jimintou_class()
-word_bbb = bbb.jimintou(args)
+aab = word_garage.jimintou_class()
+word_aab = aab.jimintou(args)
 
-if (word_bbb == "自民党"):
-    print("<a href='https://www.jimin.jp/'>" + str(word_bbb) + "</a><br><br>")
+if (word_aab == "自民党"):
+    print("<a href='https://www.jimin.jp/'>" + str(word_aab) + "</a><br><br>")
 
 # 00000000000000000000000000000000000000000000000000000000000000000000000000000000
-ccc = word_garage.lazarus_class()
-word_ccc = ccc.lazarus(args)
+aac = word_garage.lazarus_class()
+word_aac = aac.lazarus(args)
 
-if (word_ccc == "ラザルス"):
-    print("<a href='./box/lazarus.html'>" + str(word_ccc) + "</a><br><br>")
+if (word_aac == "ラザルス"):
+    print("<a href='./box/lazarus.html'>" + str(word_aac) + "</a><br><br>")
+
+# 00000000000000000000000000000000000000000000000000000000000000000000000000000000
+aad = word_garage.vermeer_class()
+word_aad = aad.vermeer(args)
+
+if (word_aad == "フェルメール"):
+    print("<a href='./box/vermeer.html'>" + str(word_aad) + "</a><br><br>")
+
+# 00000000000000000000000000000000000000000000000000000000000000000000000000000000
+aae = word_garage.deathnote_class()
+word_aae = aae.deathnote(args)
+
+if (word_aae == "デスノート"):
+    print("<a href='./box/deathnote.html'>" + str(word_aae) + "</a><br><br>")
--- word_garage.py
+++ word_garage.py
@@ -34,3 +34,27 @@ class lazarus_class():                    # クラス名、関数名は単語と
                 return "ラザルス"           #　代表的なものを一つ決める
 
 # -----------------------------------------------------------------------
+class vermeer_class():                    # クラス名、関数名は単語と同じ
+    def vermeer(self, search_word):
+        path = "./data/vermeer"                # パスを変える(単語と同じ)
+        with open(path) as f:
+            l = f.readlines()
+        for i in range(len(l)):
+            l[i] = l[i][:-1]
+        for i in range(len(l)):
+            if(search_word == l[i]):
+                return "フェルメール"           #　代表的なものを一つ決める
+
+# -----------------------------------------------------------------------
+class deathnote_class():                    # クラス名、関数名は単語と同じ
+    def deathnote(self, search_word):
+        path = "./data/deathnote"                # パスを変える(単語と同じ)
+        with open(path) as f:
+            l = f.readlines()
+        for i in range(len(l)):
+            l[i] = l[i][:-1]
+        for i in range(len(l)):
+            if(search_word == l[i]):
+                return "デスノート"           #　代表的なものを一つ決める
+
+# -----------------------------------------------------------------------
